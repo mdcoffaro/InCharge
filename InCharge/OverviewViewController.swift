9f1424f10582fa30edc98cee4b827cf183af7524
Michael Coffaro <michaelcoffaro@yahoo.com>
2015-11-25 16:55:11 -0800

    updated storyboard

--- InCharge/OverviewViewController.swift
+++ InCharge/OverviewViewController.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2015 Michael Coffaro. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Charts
+
+class OverviewViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("dong");
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
