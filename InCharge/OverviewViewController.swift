0837cf052fd095e19e27ea3c6d1e584e7530fa58
Michael Coffaro <michaelcoffaro@yahoo.com>
2015-11-28 15:20:21 -0800

    checkin

--- InCharge/OverviewViewController.swift
+++ InCharge/OverviewViewController.swift
@@ -11,12 +11,17 @@ import Charts
 
 class OverviewViewController: UIViewController {
     
-    @IBOutlet weak var textLabel: UILabel!
+    //MARK: Outlets
     @IBOutlet weak var graphTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var graphTypeLabel: UILabel!
+    
+    @IBOutlet weak var unitTypeLabel: UILabel!
+    @IBOutlet weak var unitTypeSegmentedControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("dong");
     }
     
     override func didReceiveMemoryWarning() {
@@ -24,12 +29,31 @@ class OverviewViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    //Function to handle switching between line graph and bar graph
     @IBAction func indexChanged(sender: UISegmentedControl) {
-        switch graphTypeSegmentedControl.selectedSegmentIndex{
+        switch graphTypeSegmentedControl.selectedSegmentIndex
+        {
+            case 0:
+                graphTypeLabel.text = "Line Graph"
+            case 1:
+                graphTypeLabel.text = "Bar Graph"
+            default:
+                break;
+        }
+    }
+    
+    @IBAction func unitIndexChanged(sender: UISegmentedControl) {
+        switch unitTypeSegmentedControl.selectedSegmentIndex
+        {
         case 0:
-            textLabel.text = "Line Graph"
+            unitTypeLabel.text = "Day"
         case 1:
-            textLabel.text = "Bar Graph"
+            unitTypeLabel.text = "Week"
+        case 2:
+            unitTypeLabel.text = "Month"
+        case 3:
+            unitTypeLabel.text = "Year"
         default:
             break;
         }
