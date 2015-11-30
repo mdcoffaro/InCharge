726e26823d1d2a92436e6072cd7211d28950e11b
Michael Coffaro <michaelcoffaro@yahoo.com>
2015-11-30 01:18:02 -0800

    basic line graph added

--- InCharge/OverviewViewController.swift
+++ InCharge/OverviewViewController.swift
@@ -18,6 +18,12 @@ class OverviewViewController: UIViewController {
     @IBOutlet weak var unitTypeLabel: UILabel!
     @IBOutlet weak var unitTypeSegmentedControl: UISegmentedControl!
     
+    @IBOutlet weak var lineChart: LineChartView!
+    
+    
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+    let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,13 +33,13 @@ class OverviewViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        lineChart.noDataText = "Looks like we don't have data for this month yet!"
     }
     
     
     //Function to handle switching between line graph and bar graph
     @IBAction func indexChanged(sender: UISegmentedControl) {
-        switch graphTypeSegmentedControl.selectedSegmentIndex
-        {
+        switch graphTypeSegmentedControl.selectedSegmentIndex{
             case 0:
                 graphTypeLabel.text = "Line Graph"
             case 1:
@@ -44,19 +50,35 @@ class OverviewViewController: UIViewController {
     }
     
     @IBAction func unitIndexChanged(sender: UISegmentedControl) {
-        switch unitTypeSegmentedControl.selectedSegmentIndex
-        {
-        case 0:
-            unitTypeLabel.text = "Day"
-        case 1:
-            unitTypeLabel.text = "Week"
-        case 2:
+        switch unitTypeSegmentedControl.selectedSegmentIndex{
+            case 0:
+                setChartDay(months, values: unitsSold)
+            case 1:
+                unitTypeLabel.text = "Week"
+                lineChart.noDataText = "WEEKS BRUH"
+            case 2:
             unitTypeLabel.text = "Month"
-        case 3:
-            unitTypeLabel.text = "Year"
-        default:
-            break;
+            lineChart.noDataText = "MONTHS BRUH"
+            case 3:
+                unitTypeLabel.text = "Year"
+                lineChart.noDataText = "YEARS BRUH"
+            default:
+                break;
         }
+    }
+    
+    func setChartDay(dataPoints: [String], values: [Double]) {
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+
+            
+        let lineChartDataSet = LineChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let lineChartData = LineChartData(xVals: dataPoints, dataSet: lineChartDataSet)
+        lineChart.data = lineChartData
     }
     
 }
